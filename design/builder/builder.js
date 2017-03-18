862c98313169cf902418a8fda16ac9d8f7597e97
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-17 23:40:10 -0500

    Agrego descripcion a los pasos de creacion con el patron builder

--- design/builder/builder.js
+++ design/builder/builder.js
@@ -1,4 +1,6 @@
 function testBuilderPattern() {
+  console.log("||| REVISAREMOS COMO CONSTRUIMOS UN CARRO USANDO UN" +
+              " PATRON BUILDER ||||");
   var shop = new Director()
   var carBuilder = new CarBuilder()
   var car = shop.construct(carBuilder)
@@ -7,7 +9,9 @@ function testBuilderPattern() {
 }
 
 function Director() {
+  console.log("El proceso inicia entrando al director de la construccion")
   this.construct = function(builder){
+    console.log("Se inicia la construcción")
     builder.step1()
     builder.step2()
 
@@ -16,27 +20,33 @@ function Director() {
 }
 
 function CarBuilder() {
+  console.log("Se revisa que necesita para crearse el carro")
   this.car = null
   this.step1 = function() {
+    console.log("Se ejecutan las actividades base para la creación")
     this.car = new Car();
   }
   this.step2 = function() {
+    console.log("Se agregan las partes fundamentales del carro")
     this.car.addParts()
   }
 
   this.getResult = function(){
+    console.log("Se tiene como resultado final el carro construido");
     return this.car
   }
 }
 
 function Car(){
+  console.log("Inicia el proceso de ensamblado");
   this.doors = 0
   this.addParts = function() {
+    console.log("Agregamos las puertas");
     this.doors = 4
   }
 
   this.doSomething = function() {
-    console.log("Tengo " + this.doors + " puertas");
+    console.log("Tenemos un carro con " + this.doors + " puertas");
   }
 }
 
